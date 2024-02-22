ec3fcefff537ee03b335f3953071e78ba5e43f2b
alparslanu6347 <alparslanu6347@gmail.com>
2024-02-22 13:00:18 +0300

    top-level-blocks-samples.tf updated

--- 02-Terraform-Basics/02-03-Terraform-Language-Syntax/terraform-manifests/top-level-blocks-samples.tf
+++ 02-Terraform-Basics/02-03-Terraform-Language-Syntax/terraform-manifests/top-level-blocks-samples.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 5.0"
     }
   }
   # Adding Backend as S3 for Remote State Storage with State Locking
